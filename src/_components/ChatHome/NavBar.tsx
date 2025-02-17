945c5398f5f590504ac4ad646e4e3ce11a0bc227
EljiahR <kcerhajile@gmail.com>
2025-02-17 17:24:08 -0500

    Started the structure for some sub menues in NavBar

--- src/_components/ChatHome/NavBar.tsx
+++ src/_components/ChatHome/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import instance from "../../_lib/axiosBase";
 import { Friend, UserInfo } from "../../_lib/responseTypes";
+import { useState } from "react";
 
 interface NavBarProps {
     userInfo: UserInfo
@@ -10,7 +11,18 @@ interface FriendsDisplayProps {
     friends: Friend[]
 }
 
+enum SubMenuOptions {
+    People,
+    Friends,
+    None
+}
+
 const NavBar = ({userInfo}: NavBarProps) => {
+    const [subMenu, setSubMenu] = useState<SubMenuOptions>(SubMenuOptions.None);
+    const handleSubMenu = (option: SubMenuOptions) => {
+        setSubMenu(option);
+    }
+    
     const navigate = useNavigate();
 
     const handleLogout = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -26,19 +38,36 @@ const NavBar = ({userInfo}: NavBarProps) => {
     }
     
     return (
+        <>
         <div id="nav-bar">
-            <button id="people-btn">People</button>
-            <button id="friends-btn" disabled>Friends</button>
+            <button id="people-btn" onClick={() => handleSubMenu(SubMenuOptions.People)}>People</button>
+            <button id="friends-btn" onClick={() => handleSubMenu(SubMenuOptions.Friends)} disabled>Friends</button>
             <button id="profile-btn" disabled>Profile</button>
             <button id="signout-btn" onClick={(e) => handleLogout(e)}>Sign Out</button>
         </div>
+        {subMenu == SubMenuOptions.People ? 
+            <></> :
+        subMenu == SubMenuOptions.Friends ?
+            <FriendsDisplay friends={userInfo.friends} /> :
+            <></>
+        }
+        </>
+        
     )
 }
 
 const FriendsDisplay = ({friends}: FriendsDisplayProps) => {
     
     
-    return <></>
+    return (
+        <div id="friend-list">
+            {friends.map(friend => {
+                return (
+                    <div>{friend.userName}</div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default NavBar;
