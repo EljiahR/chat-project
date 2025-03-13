933d1f902717792488a358c34319392bfcfbc581
EljiahR <kcerhajile@gmail.com>
2025-03-13 18:20:16 -0400

    Added owner/members to channel submenu

--- src/_components/ChatHome/ChannelMenu.tsx
+++ src/_components/ChatHome/ChannelMenu.tsx
@@ -1,9 +1,25 @@
+import { Channel } from "../../_lib/responseTypes";
 import "../../_styles/ChannelMenu.css"
 
-const ChannelMenu = () => {
+interface Props {
+    channel: Channel
+}
+
+const ChannelMenu = ({channel}: Props) => {
     return (
         <div id="channel-menu">
-
+            <h3>Owner</h3>
+            <div id="channel-owner">
+                <p>{channel.owner.userName}</p>
+            </div>
+            <h3>Members</h3>
+            <div id="channel-members">
+                {channel.members.concat(channel.admins).map(u => {
+                    return (
+                        <p>{u.userName}</p>
+                    )
+                })}
+            </div>
         </div>
     )
 }
