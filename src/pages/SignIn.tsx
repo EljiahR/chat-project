57c7e7f6dacffe59dea99db534b6f96fb58cd83f
EljiahR <kcerhajile@gmail.com>
2025-03-20 15:01:58 -0400

    Installed tailwind, deleted all bootstrap components in SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import instance from "../_lib/axiosBase";
-import { Button, Form, Stack } from "react-bootstrap";
 
 const SignIn: React.FC = () => {
     const [loginCredentials, setLoginCredentials] = useState({
@@ -69,43 +68,29 @@ const SignIn: React.FC = () => {
     };
     
     return (
-        <Stack id="signin-page"className="max-h-100 py-2 d-flex justify-content-center align-items-center">
+        <div id="signin-page"className="max-h-100 py-2 d-flex justify-content-center align-items-center">
             <h3>Elijah's Chat Project</h3>
             <div className="w-100 max-w-md py-2 px-3 d-flex flex-column gap-3">
-                <Form onSubmit={(e) => handleLoginSubmit(e)} id="signin-form">
-                    <Stack gap={2} className="col-md-5 mx-auto">
-                        <h4 className="align-self-center">Sign In</h4>
-                        <Form.Group controlId="login-username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter your username..." onChange={(e) => handleLoginChange("userName", e.target.value)} />
-                        </Form.Group>
-                        <Form.Group controlId="login-password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" onChange={(e) => handleLoginChange("password", e.target.value)} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">Sign In</Button>
-                    </Stack>
-                </Form>
+                <form onSubmit={(e) => handleLoginSubmit(e)} id="signin-form">
+                    <h4 className="align-self-center">Sign In</h4>
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter your username..." onChange={(e) => handleLoginChange("userName", e.target.value)} />
+                    <label>Password</label>
+                    <input type="password" placeholder="Password" onChange={(e) => handleLoginChange("password", e.target.value)} />
+                    <button type="submit">Sign In</button>
+                </form>
                 
                 
-                <Form id="register-form" onSubmit={(e) => handleRegisterSubmit(e)}>
-                    <Stack gap={2} className="col-md-5 mx-auto">
-                        <h4 className="align-self-center">Register</h4>
-                        <Form.Group controlId="register-username">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter your username..." name="userName" value={registerCredentials["userName"]} onChange={(e) => handleRegisterChange("userName", e.target.value)} />
-                        </Form.Group>
-                        <Form.Group controlId="register-password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" name="password" value={registerCredentials["password"]} onChange={(e) => handleRegisterChange("password", e.target.value)} />
-                        </Form.Group>
-                        <Form.Group controlId="register-repeat-password">
-                            <Form.Label>Repeat Password</Form.Label>
-                            <Form.Control type="password" placeholder="Repeat password" name="repeat-password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">Register</Button>
-                    </Stack>
-                </Form>
+                <form id="register-form" onSubmit={(e) => handleRegisterSubmit(e)}>
+                    <h4 className="align-self-center">Register</h4>
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter your username..." name="userName" value={registerCredentials["userName"]} onChange={(e) => handleRegisterChange("userName", e.target.value)} />
+                    <label>Password</label>
+                    <input type="password" placeholder="Password" name="password" value={registerCredentials["password"]} onChange={(e) => handleRegisterChange("password", e.target.value)} />
+                    <label>Repeat Password</label>
+                    <input type="password" placeholder="Repeat password" name="repeat-password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} />
+                    <button type="submit">Register</button>
+                </form>
                 <div id="register-errors">
                     {!passwordsMatch ? 
                         <p>Passwords don't match</p> :
@@ -113,7 +98,7 @@ const SignIn: React.FC = () => {
                     }
                 </div>
             </div>
-        </Stack>
+        </div>
         
     )
 }
