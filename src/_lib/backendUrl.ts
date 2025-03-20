5dbb83c822fd10c1b1d8b836ecf5f319b33e1fd0
EljiahR <kcerhajile@gmail.com>
2025-03-20 10:07:08 -0400

    Made testing between different environments easier

--- src/_lib/backendUrl.ts
+++ src/_lib/backendUrl.ts
@@ -1,3 +1,4 @@
-const backendUrl = "https://chatproject-reck.onrender.com";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const backendUrl = BACKEND_URL != "" && BACKEND_URL != null && BACKEND_URL != undefined ? BACKEND_URL : "https://chatproject-reck.onrender.com";
 
 export default backendUrl;
