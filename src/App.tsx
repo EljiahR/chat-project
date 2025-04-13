eb679daa2b41f710661077f6b8b509af96d3d954
EljiahR <kcerhajile@gmail.com>
2025-04-12 20:24:22 -0400

    Modified styling for LoadingScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ChatHomePage from './pages/ChatHomePage'
 import SignInPage from './pages/SignInPage'
+import LoadingScreen from './_components/Generics/LoadingScreen'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<SignInPage />} />
+        <Route path='/' element={<LoadingScreen />} />
         <Route path="/chat" element={<ChatHomePage />} />
       </Routes>
     </BrowserRouter>
--- src/_components/Generics/LoadingScreen.tsx
+++ src/_components/Generics/LoadingScreen.tsx
@@ -1,11 +1,11 @@
-import { loadingSpinnerStyle } from "../../_lib/tailwindShortcuts";
-import LoadingSpinner from "../_lib/svgs/LoadingSpinner.svg?react";
+import { loadingPageStyle, pageLoadingSpinnerStyle } from "../../_lib/tailwindShortcuts";
+import LoadingSpinner from "../../_lib/svgs/LoadingSpinner.svg?react";
 
 const LoadingScreen = () => {
     return (
-        <div>
-            <LoadingSpinner className={loadingSpinnerStyle} />
-            <h1>Validating User...</h1>
+        <div className={loadingPageStyle}>
+            <LoadingSpinner className={pageLoadingSpinnerStyle} />
+            <h2>Validating User...</h2>
         </div>
     )
 };
