56e7cf6a5546fe592c6fff9daacbc02095ba1118
EljiahR <kcerhajile@gmail.com>
2025-04-12 20:41:19 -0400

    Added time based ellipses amount to text in LoadingScreen

--- src/_components/Generics/LoadingScreen.tsx
+++ src/_components/Generics/LoadingScreen.tsx
@@ -1,11 +1,25 @@
 import { loadingPageStyle, pageLoadingSpinnerStyle } from "../../_lib/tailwindShortcuts";
 import LoadingSpinner from "../../_lib/svgs/LoadingSpinner.svg?react";
+import { useEffect, useState } from "react";
 
 const LoadingScreen = () => {
+    const [ellipses, setEllipses] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setEllipses(prev => prev.length < 3 ? prev + "." : "");
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+
+    
     return (
         <div className={loadingPageStyle}>
             <LoadingSpinner className={pageLoadingSpinnerStyle} />
-            <h2>Validating User...</h2>
+            <h2>Validating User{ellipses}</h2>
         </div>
     )
 };
