e9335baa38d2b446e089c17ec34b55157fac3c1c
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-04-22 16:29:25 +0000

    Added some animation to the notification bubble

--- src/_lib/tailwindShortcuts.ts
+++ src/_lib/tailwindShortcuts.ts
@@ -25,7 +25,7 @@ export const buttonStyleLightDisabled = buttonStyleLight + " cursor-not-allowed"
 export const buttonStyleRed = "relative focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900";
 export const buttonStyleRedSmall = "relative focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium text-xsm px-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900";
 
-export const notificationBubble = "absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-blue-500 border-2 border-white rounded-full -top-1 -end-1 dark:border-gray-900";
+export const notificationBubble = "absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-blue-500 border-2 border-white rounded-full -top-1 -end-1 dark:border-gray-900 notification-bubble";
 
 // Inputs
 export const textInputStyle = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block mw-60 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500";
