de96dec8310a3034e468f1de35e33970b43188bb
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-04-27 15:20:49 +0000

    Set background color and inset box-shadow for chat  messages

--- src/_components/ChatHome/Chat.tsx
+++ src/_components/ChatHome/Chat.tsx
@@ -11,13 +11,13 @@ const Chat: React.FC<Props> = ({chatMessages, handleChannelMenuDisplay}) => {
     const channelName = useAppSelector((state) => state.userInfo.channels.entities[state.chatUi.selectedChannelId].name);
 
     return (
-            <div id="chat" className="h-full flex flex-col">
+            <div id="chat" className="h-full flex flex-col gap-1">
                 <div id="chat-header" className="hidden sm:inline">
                     <h1 id="title">{channelName}</h1>
                     <button className={buttonStyleBlue} onClick={() => handleChannelMenuDisplay()}>Options</button>
                 </div>
                 
-                <div id="chat-box" className="grow overflow-y-auto h-50 flex flex-col-reverse pb-5">
+                <div id="chat-box" className="grow overflow-y-auto h-50 flex flex-col-reverse pb-5 bg-gray-700 rounded p-1 inset-shadow-sm inset-shadow-black">
                     {chatMessages}
                 </div>
                 <MessageControls />
